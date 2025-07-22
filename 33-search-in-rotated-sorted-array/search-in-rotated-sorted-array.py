db62d05a3b95ecd593f1219a9a698026b4afda4d
Kamalakannan M <kamalakannan.m1208@gmail.com>
2025-07-22 16:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (70.92%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        s=0
+        e=len(nums)-1
+        while(s<=e):
+            mid=(s+e)//2
+    
+            if target== nums[mid]:
+                return mid
+            elif nums[s]<=nums[mid]:
+                if(nums[s]<=target<nums[mid]):
+                    e=mid-1
+                else:
+                    s=mid+1
+            else:
+                if(nums[mid]<target<=nums[e]):
+                    s=mid+1
+                else:
+                    e=mid-1
+        return -1
+
+        
