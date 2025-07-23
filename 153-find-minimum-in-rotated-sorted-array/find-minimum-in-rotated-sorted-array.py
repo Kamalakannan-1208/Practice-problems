301752ce1c2ff1491f041b9dff02f7b279d53dde
Kamalakannan M <kamalakannan.m1208@gmail.com>
2025-07-23 16:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (92.35%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,21 @@
+import sys
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s=0
+        e=len(nums)-1
+        ans= sys.maxsize
+        while(s<=e): 
+            mid=(s+e)//2
+            if nums[s]<=nums[mid]:
+                ans=min(ans,nums[s])
+                s=mid+1
+            else:
+                ans=min(ans,nums[mid])
+                e=mid-1
+        return ans
+
+        
